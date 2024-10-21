f28a258284a2f3fb637615d8c25308993a32eb18
KevinUmude16 <kevosky16.ku@gmail.com>
2024-10-21 14:37:31 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+// Import the Express framework
+const express = require('express');
+const app = express(); // Create an Express application
+const port = 3000; // Define the port number for the server
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define a route that greets the user by name and surname
+app.get('/hello/:name/:surname', (req, res) => {
+    const name = req.params.name; // Extract the 'name' parameter from the URL
+    const surname = req.params.surname; // Extract the 'surname' parameter from the URL
+    res.send(`Hello ${name} ${surname}`); // Send a greeting message as a response
+});
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to Data Representation & Querying'); // Welcome message for the root
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack trace
+    res.status(500).send('Something went wrong!'); // Send a generic error message to the client
+});
+
+// Define a route to get a list of movies
+app.get('/api/movies', (req, res) => {
+    // Sample movie data
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(200).json({ whatever:movies }); // Send the movie data as a JSON response
+});
+
+app.get ('/index',(req, res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post ('/name',(req, res)=>{
+    res.send('Goodbye '+ req.body.firstname+" "+req.body.lastname);
+})
+
+// Define a route that greets the user based on query parameters
+app.get('/name', (req, res) => {
+    res.send('Hello '+ req.query.firstname+" "+req.query.lastname) // Send a greeting message using query parameters
+});
+
+app.post('/name', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    res.send(`Hello ${firstname} ${lastname}`);
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`); // Log the server status
+});
